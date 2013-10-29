4aed320c7775dfa386a55bf4d8c506adf98215d3
keithalewis <kal@kalx.net>
2013-10-29 07:58:59 -0400

    major refactoring

--- test/test.h
+++
@@ -1,8 +0,0 @@
-// test.h - global header for testing
-#pragma once
-//#define EXCEL12
-#include "../xll/xll.h"
-
-typedef xll::traits<XLOPERX>::xchar xchar;
-typedef xll::traits<XLOPERX>::xstring xstring;
-typedef xll::traits<XLOPERX>::xword xword;
