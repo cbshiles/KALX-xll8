c6447eabbfc9428a0d5390c1ea8f654901d426c4
keithalewis <kal@kalx.net>
2014-03-01 09:43:51 -0500

    add RANGE for Excel12

--- xll/range.cpp
+++ xll/range.cpp
@@ -36,3 +36,37 @@ extern "C" LPOPER __declspec(dllexport) WINAPI xll_range_get(HANDLEX h)
 
 	return po;
 }
+
+static AddIn12 xai_range_set12(
+	Function12(XLL_HANDLE12, L"_xll_range_set12@4", L"RANGE.SET")
+	.Arg(XLL_LPOPER12, L"Range", L"is a range.")
+	.Uncalced()
+	.Category(L"XLL")
+	.FunctionHelp(L"Return a handle to a range.")
+);
+extern "C" HANDLEX __declspec(dllexport) WINAPI xll_range_set12(LPOPER12 po)
+{
+	handle<OPER12> ho(new OPER12(*po));
+
+	return ho.get();
+}
+
+static AddIn12 xai_range_get12(
+	Function12(XLL_LPOPER12, L"_xll_range_get12@8", L"RANGE.GET")
+	.Arg(XLL_HANDLE12, L"Handle", L"is a handle to a range.")
+	.Category(L"XLL")
+	.FunctionHelp(L"Return a range corresponding to handle.")
+);
+extern "C" LPOPER12 __declspec(dllexport) WINAPI xll_range_get12(HANDLEX h)
+{
+	LPOPER12 po(0);
+
+	try {
+		po = handle<OPER12>(h).ptr();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return po;
+}
