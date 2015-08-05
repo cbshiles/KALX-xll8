cc9b25a7eddaa4cbf67e3654c00ffaa11b6ec956
Keith A. Lewis <kal@kalx.net>
2015-08-05 09:01:05 -0400

    help files

--- xll/shfb/shfb.cpp
+++ xll/shfb/shfb.cpp
@@ -2,11 +2,12 @@
 // Create files for building help files.
 #include <iostream>
 #include <fstream>
-#ifdef EXCEL12
-#undef EXCEL12
-#endif
-
+#include <map>
+#define EXCEL12
 #include "../xll.h"
+
+using namespace std;
+using namespace xll;
 
 #include "shfbproj.txt"
 
@@ -26,14 +27,26 @@ namespace shfb {
 
 using namespace xll;
 
+template<class X>
+void xll_make_one(const XAddIn<X>* pai)
+{
+
+}
+
 extern "C" int __declspec(dllexport) WINAPI xll_make_doc()
 {
 	try {
+		map<string,map<string,const XAddIn<XLOPER>*>> cat_fun_map;
 		// get list of all addins
+		for (const auto& i : XAddIn<XLOPER>::List()) {
+			cat_fun_map[i->Args().Category().string()][i->Args().FunctionText().string()] = i;
+		}
+
+		auto list12 = XAddIn<XLOPER12>::List();
+		for (const auto& i : list12)
+			xll_make_one<XLOPER12>(i);
+
 		// generate shfbproj file
-		double x;
-		x = 1;
-		ensure (x == 1);
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
